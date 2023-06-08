c94bda27952d1d346d6aa73891265bb059513d14
tajbaba999 <tajbaba153@gmail.com>
2023-06-08 19:10:44 +0530

    Recent commits

---
+++ expended_widget/lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 50,
+            height: 50,
+            color: Colors.lightBlue,
+          ),
+          Container(
+            width: 50,
+            height: 50,
+            color: Colors.lime,
+          ),
+          Container(
+            width: 50,
+            height: 50,
+            color: Colors.cyanAccent,
+          ),
+          Expanded(
+            child: Container(
+              width: 50,
+              height: 50,
+              color: Colors.deepOrange,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
