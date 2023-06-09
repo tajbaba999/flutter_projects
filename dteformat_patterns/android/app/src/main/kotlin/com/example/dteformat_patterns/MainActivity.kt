dac18e50d3581a97d5cb0a0f6015fc237904e35d
tajbaba999 <tajbaba153@gmail.com>
2023-06-09 23:35:38 +0530

    Date format intl package

---
+++ dteformat_patterns/android/app/src/main/kotlin/com/example/dteformat_patterns/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dteformat_patterns
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ dteformat_patterns/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    var date = DateTime.now();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Date and Time'),
+        ),
+        body: Text('Date for :${DateFormat}',style: TextStyle(fontSize: 35),)
+    );
+
+  }
+}
