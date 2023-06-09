c41bb045b2ecf514dd03ae5e940be36a054c6581
tajbaba999 <tajbaba153@gmail.com>
2023-06-09 22:59:04 +0530

    total know how to user-input

--- user_input/lib/main.dart
+++ user_input/lib/main.dart
@@ -33,16 +33,59 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
+  var email = TextEditingController();
+  var pass = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
 
     return Scaffold(
       appBar: AppBar(
-
+      title: Text('User input'),
       ),
       body: Center(
+        child: Container(
+          width:300,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                keyboardType: TextInputType.phone,
+                controller: email,
+                decoration: InputDecoration(
+
+                  focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(21),
+            borderSide: BorderSide(
+              color: Colors.deepOrange,
+                  ),
+                ),
+              ),
+              ),
+              Container(height: 11,),
+              TextField(
+                controller: pass,
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(21),
+                        borderSide: BorderSide(
+                          color: Colors.deepOrange,
+                        ),
+                    ),
+                ),
+              ),
+              ElevatedButton(onPressed: (){
+                  String uname = email.text.toString();
+                  String upass = pass.text.toString();
+                  
+                    print("Email : $email Password: $pass");
+              }, child: Text(
+                'Login'
+              ))
+            ],
+          ),
+        ),
       ),
     );
 
