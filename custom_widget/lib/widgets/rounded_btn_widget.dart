8b6c3a85db1558c599367ee612bc565c982183ae
tajbaba999 <tajbaba153@gmail.com>
2023-06-11 10:42:30 +0530

    Wrap widget

---
+++ custom_widget/lib/widgets/rounded_btn_widget.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class RoundedButton extends StatelessWidget{
+    final String btnName;
+    final Icon? icon;
+    final Color? bgColor;
+    final TextStyle? textStyle;
+    final VoidCallback? callback;
+
+    RoundedButton({
+      required this.btnName,
+      this.icon,
+      this.bgColor=Colors.blue,
+      this.textStyle,
+      this.callback});
+
+    @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+          onPressed: (){
+            callback!();
+          }, child: icon!=null ? Row(
+        children: [
+          icon!,
+          Text(btnName, style: textStyle,)
+        ],
+
+      ) : Text(btnName, style: textStyle,),
+        style: ElevatedButton.styleFrom(
+         primary: bgColor,
+         shadowColor:  bgColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(21),
+                bottomLeft: Radius.circular(21),
+              )
+            )
+        ),
+      ),
+    );
+  }
+
+}
