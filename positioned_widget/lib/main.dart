6f13f2fd0f595b7d852e77b1ec2e2ee4ed9684ad
tajbaba999 <tajbaba153@gmail.com>
2023-06-12 18:22:01 +0530

    Positioned widget

---
+++ positioned_widget/lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+
+        title: Text(widget.title),
+      ),
+      body: Container(
+        width: 300,
+        height: double.infinity,
+        color: Colors.indigo,
+        child: Stack(
+            children: [
+              Positioned(
+                bottom: 110,
+                right: 11,
+
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.deepOrangeAccent,
+                ),
+              )
+            ],
+        ),
+      ),
+    );
+  }
+}
