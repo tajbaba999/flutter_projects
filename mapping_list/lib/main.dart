2d3a420606bf35c6c15f036598e17a7d11f96d9c
tajbaba999 <tajbaba153@gmail.com>
2023-06-18 01:10:34 +0530

    mapping list

---
+++ mapping_list/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  var arrData = [
+    'Rosy',
+    'john',
+    'Virat',
+    'nikhil',
+    'nithin',
+    'kailsh',
+    'suzee',
+    'kayte'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        child: ListView(
+          children : arrData.map((value){
+            return Container(
+              color: Colors.blue.shade800,
+              child: Text(value),
+            );
+          }).toList()
+        ),
+      ),
+    );
+  }
+}
